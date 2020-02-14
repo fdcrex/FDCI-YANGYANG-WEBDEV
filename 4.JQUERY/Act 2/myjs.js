a87c5424af7fe2ad3aec62c4ae1b438cfbc62cf1
fdcrex <50431266+fdcrex@users.noreply.github.com>
2020-02-14 10:08:05 +0800

    Act 2: Javascript myjs file update

--- 4.JQUERY/Act 2/myjs.js
+++ 4.JQUERY/Act 2/myjs.js
@@ -3,60 +3,73 @@ var errorMsg = "";
 
 function longWord(){
 
-	// ifEmpty(document.getElementById("words").value, document.getElementById("errorMsg1").getAttribute("id"));
+	var str = document.getElementById("words").value;
+	var con = document.getElementById("errorMsg1").getAttribute("id");
+	var ans = document.getElementById("answerPart1").getAttribute("id");
 
-	var text = document.getElementById("words").value.split(' '); //will return an array of words that was separated by space
+	var text = str.split(' '); //will return an array of words that was separated by space
 	var wordLen = 0;
 	var longestWord = "";
 
-	for(i = 0; i < text.length; i++){
-		if(text[i].length > wordLen){ 
-			wordLen = text[i].length;
-			longestWord = text[i];
-	     }
-	}
+	if(ifEmpty(str)){
+		exe(con, ans);
+	}else{
+
+		for(i = 0; i < text.length; i++){
+			if(text[i].length > wordLen){ 
+				wordLen = text[i].length;
+				longestWord = text[i];
+		     }
+		}
 	
-	document.getElementById("answerPart1").innerHTML = longestWord;
+		document.getElementById(ans).innerHTML = longestWord;
+		document.getElementById(con).innerHTML = "";
+	}
 }
 
 function primeOrNot(){
 
-	/*var num = document.getElementById("number").value;
-	ifEmpty(num, document.getElementById("errorMsg2").getAttribute("id"));*/
+	var num = document.getElementById("number").value;
+	var con = document.getElementById("errorMsg2").getAttribute("id");
+	var ans = document.getElementById("answerPart2").getAttribute("id");
 
-
-	if (isNaN(num)){
-
-		errorMsg = "Please input a number.";
-		document.getElementById("errorMsg2").innerHTML = errorMsg;
-
+	if(ifEmpty(num)){
+		exe(con, ans);
 	}else{
 
-		var res = "";
-		if(num == 1){
+		if (isNaN(num)){
 
-			res = num + " is not a prime number";
-			document.getElementById("answerPart2").innerHTML = res;
-			document.getElementById("errorMsg2").innerHTML = "";
-
-		}else if(num == 2){
-
-			res = num + " is prime number";
-			document.getElementById("answerPart2").innerHTML = res;
-			document.getElementById("errorMsg2").innerHTML = "";
+			errorMsg = "Please input a number.";
+			document.getElementById(con).innerHTML = errorMsg;
 
 		}else{
 
-			for(i = 2; i < num; i++){
-				if(num % i == 0){
-					res = num + " is not a prime number";
-					break;
-				}else{
-					res = num + " is prime number";
+			var res = "";
+			if(num == 1){
+
+				res = num + " is not a prime number";
+				document.getElementById(ans).innerHTML = res;
+				document.getElementById(con).innerHTML = "";
+
+			}else if(num == 2){
+
+				res = num + " is prime number";
+				document.getElementById(ans).innerHTML = res;
+				document.getElementById(con).innerHTML = "";
+
+			}else{
+
+				for(i = 2; i < num; i++){
+					if(num % i == 0){
+						res = num + " is not a prime number";
+						break;
+					}else{
+						res = num + " is prime number";
+					}
 				}
+				document.getElementById(ans).innerHTML = res;
+				document.getElementById(con).innerHTML = "";
 			}
-			document.getElementById("answerPart2").innerHTML = res;
-			document.getElementById("errorMsg2").innerHTML = "";
 		}
 	}
 }
@@ -65,25 +78,32 @@ function convertMins(e){
 	e.preventDefault();
 
 	var num = document.getElementById("hour").value;
-	if(num === ""){
-		errorMsg = "This field can not be empty.";
-		document.getElementById("errorMsg3").innerHTML = errorMsg;
-		document.getElementById("answerPart3").innerHTML = "";
+	var con = document.getElementById("errorMsg3").getAttribute("id");
+	var ans = document.getElementById("answerPart3").getAttribute("id");
+
+	if(ifEmpty(num)){
+		exe(con, ans);
 	}else{
 
 		var mins = (num * 60) + " minutes";
-		document.getElementById("answerPart3").innerHTML = mins;
-
-		errorMsg = "";
-		document.getElementById("errorMsg3").innerHTML = errorMsg;
+		document.getElementById(ans).innerHTML = mins;
+		document.getElementById(con).innerHTML = "";
 
 		return false;
 	}
 }
 
-function ifEmpty(val, con){
+function ifEmpty(val){
 	if(val === ""){
-		errorMsg = "This field can not be empty.";
-		document.getElementById(con).innerHTML = errorMsg;
+		return true;
+	}else{
+		return false;
 	}
+}
+
+function exe(con, ans){
+	errorMsg = "This field can not be empty.";
+
+	document.getElementById(con).innerHTML = errorMsg;
+	document.getElementById(ans).innerHTML = "";
 }
